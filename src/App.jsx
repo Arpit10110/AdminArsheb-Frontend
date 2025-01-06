fdb7abf9118e33d221461b70741fb492b75c3c78
Arpit <omagrahari55@gmail.com>
2025-01-07 01:32:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {HashRouter as Router , Routes , Route} from "react-router-dom"
+import Home from "./Pages/Home.jsx"
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}  />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
