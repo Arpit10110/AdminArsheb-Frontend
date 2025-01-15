8a4661cf714cf824665e69eae14ae4f4324d0bb4
Arpit <omagrahari55@gmail.com>
2025-01-15 23:12:13 +0530

    table view created for the pending orders

---
+++ src/Components/Datatable.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import { Link } from '@mui/material';
+const Datatable = ({date,sn,payment,address,total}) => {
+    const columns = [
+        { id: 'sn', label: 'SN.', minWidth: 50 },
+        { id: 'Date', label: 'Date & Time', minWidth: 50 },
+        // { id: 'customer_name', label: 'Customer Name', minWidth: 50 },
+        { id: 'customer_address', label: 'Customer Address', minWidth: 50 },
+        { id: 'total', label: 'Total Amount', minWidth: 50 },
+        { id: 'payment', label: 'Payment', minWidth: 50 },
+        { id: 'status', label: 'Status', minWidth: 50 },
+        { id: 'view_more', label: 'View More', minWidth: 50 },
+      ];
+
+      const rows = [
+        { 
+          sn: sn,
+        //   customer_name: user.name,
+        customer_address: address ,
+        Date:date,
+        payment: payment,
+        total: `₹${total}`,
+        status:"🟠Pending" 
+         }
+      ];
+
+
+  return (
+   <>
+     <TableBody>
+    {rows.map((row) => (
+      <TableRow hover key={row.sn}>
+        {columns.map((column) => (
+          <TableCell className='!text-[1.2rem]  ' key={`${row.sn}-${column.id}`}>
+            {
+              column.id == "view_more" ? 
+              <Link className=' text-blue-500   '  href={`/`} >click here</Link>:
+              row[column.id]
+            }
+          </TableCell>
+        ))}
+      </TableRow>
+    ))}
+  </TableBody>
+   </>
+  )
+}
+
+export default Datatable
--- src/Components/PandingOrders.jsx
+++ src/Components/PandingOrders.jsx
@@ -1,26 +1,78 @@
 import React,{useState,useEffect} from 'react'
 import axios from "axios"
+import Datatable from './Datatable';
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 const PandingOrders = () => {
     const [isLoading,SetIsLoading] = useState(true);
     const [PendingList,SetPendingList] = useState([])
+
+    const columns = [
+        { id: 'sn', label: 'SN.', minWidth: 50 },
+        { id: 'Date', label: 'Date & Time', minWidth: 50 },
+        // { id: 'customer_name', label: 'Customer Name', minWidth: 50 },
+        { id: 'customer_address', label: 'Customer Address', minWidth: 50 },
+        { id: 'total', label: 'Total Amount', minWidth: 50 },
+        { id: 'payment', label: 'Payment', minWidth: 50 },
+        { id: 'status', label: 'Status', minWidth: 50 },
+        { id: 'view_more', label: 'View More', minWidth: 50 },
+      ];
 
         const pendingdata = async()=>{
         try {
             const {data} = await axios.get(`${import.meta.env.VITE_Port}/pendingorders`);
             console.log(data);
-            
+            SetPendingList(data.data);
+            SetIsLoading(false)
         } catch (error) {
             console.error(error)
         }
     }
 
-// mann nhi hai aaj
     useEffect(() => {
      pendingdata();
     }, [])
     
   return (
     <>
+    {
+        isLoading?<div className='text-[4rem] font-semibold flex justify-center items-center min-h-[60vh]'>
+            <h1>Loading....</h1>
+        </div>:
+    <div className='w-full pt-[2rem] flex flex-wrap justify-around gap-y-[3rem] pb-[5rem] mt-[3rem]' >
+               <Paper sx={{ width: '95%', overflow: 'visible'  }}>
+               <TableContainer sx={{ maxHeight: 'none' }}>
+                    <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow >
+                        {columns.map((column) => (
+                            <TableCell
+                            className='!text-[1.4rem] !font-semibold !text-center '
+                            key={column.id}
+                            align={column.align || 'left'}
+                            style={{ minWidth: column.minWidth }}
+                            >
+                            {column.label}
+                            </TableCell>
+                        ))}
+                        </TableRow>
+                    </TableHead>
+                    {
+                PendingList.map((i,index)=>{
+                    return(
+                        <Datatable key={index} date={i.orderdate} sn={index+1} total={i.totalprice} payment={i.payment} address={i.address}/>
+                    )
+                })
+            }
+                    </Table>
+               </TableContainer>
+               </Paper>
+    </div>
+    }
     </>
   )
 }
